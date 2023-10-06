efb75c2d59472aab023e6ab604db5d23ce642995
dudasdora <dudasdora06@gmail.com>
2023-10-06 16:43:34 +0200

    refactored useGetContacts

--- frontend/src/hooks/useGetContacts.ts
+++ frontend/src/hooks/useGetContacts.ts
@@ -4,17 +4,15 @@ import { Contact } from '../types'
 
 const apiUrl = config.apiUrl
 
-export const useGetContacts = () => {
-  const getContacts = async () => {
-    const res = await fetch(`${apiUrl}/contact`)
-    return await res.json()
-  }
+const getContacts = async () => {
+  const res = await fetch(`${apiUrl}/contact`)
+  return await res.json()
+}
 
-  const { data, error, isLoading, refetch } = useQuery<Contact[]>({
-    queryKey: ['contact','getAll'],
+export const useGetContacts = () => {
+  return useQuery<Contact[]>({
+    queryKey: ['contact', 'list'],
     queryFn: getContacts,
     staleTime: 60000
   })
-
-  return { data, error, isLoading, refetch }
 }
