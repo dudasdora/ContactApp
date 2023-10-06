ccb2cb6fa7f1e7b5ec7655008d930bb2d933b9b8
dudasdora <dudasdora06@gmail.com>
2023-10-06 16:34:41 +0200

    refactored usePicture

--- frontend/src/hooks/usePicture.ts
+++ frontend/src/hooks/usePicture.ts
@@ -1,42 +1,22 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useMutation } from 'react-query'
-import config from '../config'
-
-const apiUrl = config.apiUrl
+import { uploadPicture } from '../utils/uploadPicture'
 
 export const usePicture = (currentPictureUrl: string) => {
   const [file, setFile] = useState(new Blob())
   const [pictureUrl, setPictureUrl] = useState<string>(currentPictureUrl)
 
-  const uploadPicture = async () => {
-    const formData = new FormData()
-    formData.append('file', file)
-
-    try {
-      const response = await axios.post(`${apiUrl}/image`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-
-      setPictureUrl(response.data.pictureUrl)
-    } catch (error) {
-      console.error('Error:', error)
-    }
-  }
-
-  const uploadPictureMutation = useMutation(async () => await uploadPicture())
-
-  const handleUpload = () => {
-    uploadPictureMutation.mutate()
-  }
+  const { mutateAsync } = useMutation(uploadPicture)
 
   useEffect(() => {
     if (file.size !== 0) {
-      handleUpload()
+      mutateAsync(file)
+        .then((response) => setPictureUrl(response.data.pictureUrl))
+        .catch((error) => {
+          console.error('Error:', error)
+        })
     }
-  }, [file])
+  }, [file, mutateAsync])
 
   return {
     setPictureUrl,
---
+++ frontend/src/utils/uploadPicture.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import config from '../config'
+
+const apiUrl = config.apiUrl
+
+export const uploadPicture = async (file: Blob) => {
+  const formData = new FormData()
+
+  formData.append('file', file)
+
+  return await axios.post(`${apiUrl}/image`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+}
