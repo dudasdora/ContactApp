04fe4b77a5523372604970e926bee2b1345e2344
dudasdora <dudasdora06@gmail.com>
2023-10-07 13:17:18 +0200

    added typograph styles

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -14,8 +14,48 @@ const theme = createTheme({
       disabled: alpha(textBase, 0.32)
     }
   },
-
-  components: {}
+  spacing: 8,
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        //  Glysa - H1 - Medium - Font size: 32px - Line height: 48px - Letter spacing: 0%
+        h1: {
+          fontFamily: 'Glysa',
+          fontSize: '32px',
+          lineHeight: '48px',
+          letterSpacing: 0
+        },
+        // Glysa - H2 - Medium - Font size: 24px - Line height: 40px - Letter spacing: 0%
+        h2: {
+          fontFamily: 'Glysa',
+          fontSize: '24px',
+          lineHeight: '40px',
+          letterSpacing: 0
+        },
+        //Lexend Deca - Regular - Font size: 16px - Line height: 24px - Letter spacing: 1%
+        h3: {
+          fontFamily: 'Lexend Deca',
+          fontSize: '16px',
+          lineHeight: '24px',
+          letterSpacing: '.1rem'
+        },
+        // Lexend Deca - Body - Regular - Font size: 14px - Line height: 20px - Letter spacing: 1%
+        body1: {
+          fontFamily: 'Lexend Deca',
+          fontSize: '14px',
+          lineHeight: '20px',
+          letterSpacing: '.1rem'
+        },
+        // Lexend Deca - Body - Regular - Font size: 12px - Line height: 12px - Letter spacing: 1%
+        body2: {
+          fontFamily: 'Lexend Deca',
+          fontSize: '12px',
+          lineHeight: '12px',
+          letterSpacing: '.1rem'
+        }
+      }
+    }
+  }
 })
 
 export default theme
