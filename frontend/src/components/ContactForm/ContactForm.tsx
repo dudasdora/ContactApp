1afd7beba996a1edef163971f4bfc0cbaddebd67
dudasdora <dudasdora06@gmail.com>
2023-10-07 22:27:04 +0200

    Added Form layout

--- frontend/src/components/ContactForm/ContactForm.tsx
+++ frontend/src/components/ContactForm/ContactForm.tsx
@@ -1,5 +1,12 @@
-import { Button, InputLabel, TextField } from '@mui/material'
-import React, { useEffect, useRef } from 'react'
+import {
+  Box,
+  Button,
+  Grid,
+  InputLabel,
+  TextField,
+  Typography
+} from '@mui/material'
+import React from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Contact, ContactFormData } from '../../types'
@@ -28,38 +35,52 @@ const ContactForm: React.FC<IContactForm> = ({
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <h2>{title}</h2>
-      <PictureUpload
-        setValue={setValue}
-        defaultPictureUrl={defaultValues.pictureUrl}
-      />
+      <Grid container direction="column" spacing={3}>
+        <Grid item>
+          <Typography variant="h2">{title}</Typography>
+        </Grid>
+        <Grid item>
+          <PictureUpload
+            setValue={setValue}
+            defaultPictureUrl={defaultValues.pictureUrl}
+          />
+        </Grid>
+        <Grid item>
+          <InputLabel>Name</InputLabel>
+          <Controller
+            name="name"
+            control={control}
+            render={({ field }) => (
+              <TextField {...field} ref={control.register('name').ref} />
+            )}
+          />
+        </Grid>
+        <Grid item>
+          <InputLabel>Phone number</InputLabel>
+          <Controller
+            name="phone"
+            control={control}
+            render={({ field }) => <TextField {...field} />}
+          />
+        </Grid>
 
-      <InputLabel>Name</InputLabel>
-      <Controller
-        name="name"
-        control={control}
-        render={({ field }) => (
-          <TextField {...field} ref={control.register('name').ref} />
-        )}
-      />
-
-      <InputLabel>Phone number</InputLabel>
-      <Controller
-        name="phone"
-        control={control}
-        render={({ field }) => <TextField {...field} />}
-      />
-
-      <InputLabel>Email address</InputLabel>
-      <Controller
-        name="email"
-        control={control}
-        render={({ field }) => <TextField {...field} />}
-      />
-      <Button variant="contained" color="primary" onClick={onClose}>
-        Cancel
-      </Button>
-      <Button type="submit">Done</Button>
+        <Grid item>
+          <InputLabel>Email address</InputLabel>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => <TextField {...field} />}
+          />
+        </Grid>
+        <Grid item>
+          <Box display="flex" justifyContent="right" paddingTop={3} gap={1}>
+            <Button variant="contained" color="primary" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button type="submit">Done</Button>
+          </Box>
+        </Grid>
+      </Grid>
     </form>
   )
 }
--- frontend/src/components/ContactForm/PictureUpload.tsx
+++ frontend/src/components/ContactForm/PictureUpload.tsx
@@ -1,8 +1,17 @@
-import { Avatar, Button, IconButton, SvgIcon } from '@mui/material'
+import {
+  Avatar,
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  SvgIcon,
+  Typography
+} from '@mui/material'
 import { useEffect, useMemo } from 'react'
 import { useGetAvatarSource } from '../../hooks/useGetAvatarSource'
 import { ReactComponent as DeleteIcon } from '../../assets/icons/Delete.svg'
 import { ReactComponent as ChangeIcon } from '../../assets/icons/Change.svg'
+import { ReactComponent as AddIcon } from '../../assets/icons/Add.svg'
 import { usePicture } from '../../hooks/usePicture'
 import { UseFormSetValue } from 'react-hook-form'
 import { ContactFormData } from '../../types'
@@ -20,40 +29,45 @@ const PictureUpload: React.FC<IPictureUpload> = ({
     setValue('pictureUrl', pictureUrl)
   }, [pictureUrl, setValue])
 
-  const addButtonText = useMemo(() => {
-    if (pictureUrl) {
-      return (
-        <>
-          <SvgIcon component={ChangeIcon} />
-          Change picture
-        </>
-      )
-    }
-    return '+ Add picture'
-  }, [pictureUrl])
-
   const src = useGetAvatarSource(pictureUrl)
   return (
-    <>
-      <Avatar src={src} />
-      <Button variant="contained" component="label">
-        {addButtonText}
-        <input
-          hidden
-          type="file"
-          onChange={(e) => {
-            if (e.target.files) {
-              setFile(e.target.files[0])
-            }
-          }}
-        />
-      </Button>
-      {!!pictureUrl && (
-        <IconButton onClick={() => setPictureUrl('')}>
-          <SvgIcon component={DeleteIcon} />
-        </IconButton>
-      )}
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs="auto">
+        <Avatar src={src} sx={{ width: '88px', height: '88px' }} />
+      </Grid>
+      <Grid item xs alignSelf="center">
+        <Box display="flex" justifyContent="space-between">
+          <Button component="label">
+            {pictureUrl ? (
+              <>
+                <SvgIcon component={ChangeIcon} />
+                <Typography>Change picture</Typography>
+              </>
+            ) : (
+              <>
+                <SvgIcon component={AddIcon} />
+                <Typography>Add picture</Typography>
+              </>
+            )}
+            <input
+              hidden
+              type="file"
+              onChange={(e) => {
+                if (e.target.files) {
+                  setFile(e.target.files[0])
+                }
+              }}
+            />
+          </Button>
+
+          {!!pictureUrl && (
+            <IconButton onClick={() => setPictureUrl('')}>
+              <SvgIcon component={DeleteIcon} />
+            </IconButton>
+          )}
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
 
