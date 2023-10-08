bc84a70429e41de680c0428e213c96c0b91d211c
dudasdora <dudasdora06@gmail.com>
2023-10-08 19:16:15 +0200

    added defaults to schema

--- frontend/src/utils/contactFormSchema.ts
+++ frontend/src/utils/contactFormSchema.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 export const contactFormSchema = yup.object().shape({
   id: yup.number().nullable(),
   name: yup.string().required('Name is requried').nullable().default(''),
-  email: yup.string().nullable().default(null),
-  phone: yup.string().nullable().default(null),
-  pictureUrl: yup.string().nullable().default(null)
+  email: yup.string().nullable().default(''),
+  phone: yup.string().nullable().default(''),
+  pictureUrl: yup.string().nullable().default('')
 })
