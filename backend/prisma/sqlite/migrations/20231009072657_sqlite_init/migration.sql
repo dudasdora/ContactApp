d0bae3cd730d615e276d5a5f3e8a643c16eb442d
dudasdora <dudasdora06@gmail.com>
2023-10-09 09:41:40 +0200

    added local db schema

---
+++ backend/prisma/sqlite/migrations/20231009072657_sqlite_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT,
+    "name" TEXT NOT NULL,
+    "phone" TEXT,
+    "pictureUrl" TEXT
+);
